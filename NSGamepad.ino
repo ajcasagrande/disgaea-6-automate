d48fd387347c4b03fdeac59ccb44163dd47a5909
Anthony Casagrande <anthonyjcasagrande@gmail.com>
2025-05-18 08:19:44 -0700

    Update license

--- NSGamepad.ino
+++ NSGamepad.ino
@@ -5,6 +5,7 @@
 /*
  * MIT License
  * 
+ * Copyright (c) 2021 Anthony Casagrande
  * Copyright (c) 2020 gdsports625@gmail.com
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
